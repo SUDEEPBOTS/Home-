4b79ed950dcc5c11d23aefb614655774417dd6f3
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-11-29 00:37:25 +0530

    Create broadcast.js

---
+++ pages/api/broadcast.js
@@ -0,0 +1,20 @@
+import dbConnect from "@/lib/db";
+import Site from "@/models/Site";
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  if (req.method !== "POST")
+    return res.status(404).json({ error: "Not found" });
+
+  const { message } = req.body;
+
+  try {
+    await dbConnect();
+    await Site.updateMany({}, { broadcast: message });
+
+    return res.status(200).json({ ok: true });
+  } catch (err) {
+    return res.status(500).json({ error: "Server error" });
+  }
+}
