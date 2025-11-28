9b627449ce490bc90dfd3921de88b06006538367
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-11-29 00:34:13 +0530

    Update Site.js

--- models/Site.js
+++ models/Site.js
@@ -4,10 +4,10 @@ const SiteSchema = new mongoose.Schema({
   siteId: { type: String, unique: true },
   siteName: String,
   siteUrl: String,
-  siteOff: Boolean,
-  botOff: Boolean,
-  disabledMessage: String,
-  broadcast: String,
+  siteOff: { type: Boolean, default: false },
+  botOff: { type: Boolean, default: false },
+  disabledMessage: { type: String, default: "" },
+  broadcast: { type: String, default: "" },
 });
 
 export default mongoose.models.Site || mongoose.model("Site", SiteSchema);
